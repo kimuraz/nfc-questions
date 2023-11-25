ad7b474093d5983e74461d23d4cde7430b0b266f
Kimuraz <j.eduardokimura@icloud.com>
2023-11-25 16:55:54 +0100

    Fix short form reactiveness

--- src/components/ShortQuestionnaire.vue
+++ src/components/ShortQuestionnaire.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-import { ref, defineEmits, Ref } from 'vue';
+import { ref, defineEmits, Ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { ShortQuestion } from '../constants/questions';
 import QuestionnaireForm from './QuestionnaireForm.vue';
 
-const { tm } = useI18n();
+const { tm, locale } = useI18n();
 const shortQuestionnaire: Ref<ShortQuestion[]> = ref((tm('short') as string[]).map((questionStr: string) => ({
     text: questionStr,
     score: 0,
 })));
 const emit = defineEmits(['shortQuestionnaire']);
+
+// This is a pitfall of using tm to generate the questionnaire.
+watch(locale, () => {
+  shortQuestionnaire.value = (tm('short') as string[]).map((questionStr: string, index: number) => ({
+    text: questionStr,
+    score: shortQuestionnaire.value[index]?.score || 0,
+  }));
+});
 
 const submit = (data: ShortQuestion[]) => {
   emit('shortQuestionnaire', data);
